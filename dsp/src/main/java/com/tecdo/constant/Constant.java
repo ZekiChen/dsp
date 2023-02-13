098da8c6f7cb8a3369fff045e580bb1dbd3b5e4b
Zeki <zeki.chen@tec-do.com>
2023-02-13 12:23:03 +0800

    [bugfix] task timeout过小，调大至50ms

--- dsp/src/main/java/com/tecdo/constant/Constant.java
+++ dsp/src/main/java/com/tecdo/constant/Constant.java
@@ -18,8 +18,8 @@ public interface Constant {
     long TIMEOUT_LOAD_DB_CACHE_AD_DTO = 10 * 1000L;
     long INTERVAL_RELOAD_DB_CACHE = 5 * 60 * 1000L;
 
-    long TIMEOUT_ADS_RECALL = 10L;
-    long TIMEOUT_PRE_DICT = 10L;
-    long TIMEOUT_CALC_PRICE = 10L;
+    long TIMEOUT_ADS_RECALL = 50L;
+    long TIMEOUT_PRE_DICT = 50L;
+    long TIMEOUT_CALC_PRICE = 50L;
     long TIMEOUT_PRICE_FILTER = 10L;
 }
