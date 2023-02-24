eca0b5e38ed4eebda01e4b40d5072262ac01369d
Zeki <zeki.chen@tec-do.com>
2023-02-24 16:38:32 +0800

    remove xxl-job, in another branch

--- dsp/src/main/java/com/tecdo/Application.java
+++ dsp/src/main/java/com/tecdo/Application.java
@@ -5,14 +5,12 @@ import com.tecdo.common.launch.PacApplication;
 import com.tecdo.constant.AppConstant;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by Zeki on 2022/12/28
  **/
 @SpringBootApplication
 @EnableApolloConfig
-@EnableScheduling
 @MapperScan("com.tecdo.mapper.**")
 public class Application {
 
