9d020967be54c25a8b5c71057609dfdcb6841bc2
Zeki <zeki.chen@tec-do.com>
2023-03-01 18:50:57 +0800

    [opt] replace dependency

--- common/src/main/java/com/tecdo/common/domain/biz/RCode.java
+++ common/src/main/java/com/tecdo/common/domain/biz/RCode.java
@@ -1,9 +1,9 @@
 package com.tecdo.common.domain.biz;
 
 
-import io.netty.handler.codec.http.HttpResponseStatus;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.apache.http.HttpStatus;
 
 /**
  * 返回值各种状态枚举
@@ -17,16 +17,16 @@ public enum RCode {
     /**
      * 常规状态码：2xx、3xx、4xx、5xx
      */
-    SUCCESS(HttpResponseStatus.OK.code(), "操作成功"),
+    SUCCESS(HttpStatus.SC_OK, "操作成功"),
 
-    UN_AUTHORIZED(HttpResponseStatus.UNAUTHORIZED.code(), "请求未授权"),
-    NOT_FOUND(HttpResponseStatus.NOT_FOUND.code(), "没找到请求"),
-    METHOD_NOT_SUPPORTED(HttpResponseStatus.METHOD_NOT_ALLOWED.code(), "不支持当前请求方法"),
-    MEDIA_TYPE_NOT_SUPPORTED(HttpResponseStatus.UNSUPPORTED_MEDIA_TYPE.code(), "不支持当前媒体类型"),
-    REQ_REJECT(HttpResponseStatus.FORBIDDEN.code(), "请求被拒绝"),
-    FAILURE(HttpResponseStatus.BAD_REQUEST.code(), "业务异常"),
+    UN_AUTHORIZED(HttpStatus.SC_UNAUTHORIZED, "请求未授权"),
+    NOT_FOUND(HttpStatus.SC_NOT_FOUND, "没找到请求"),
+    METHOD_NOT_SUPPORTED(HttpStatus.SC_METHOD_NOT_ALLOWED, "不支持当前请求方法"),
+    MEDIA_TYPE_NOT_SUPPORTED(HttpStatus.SC_UNSUPPORTED_MEDIA_TYPE, "不支持当前媒体类型"),
+    REQ_REJECT(HttpStatus.SC_FORBIDDEN, "请求被拒绝"),
+    FAILURE(HttpStatus.SC_BAD_REQUEST, "业务异常"),
 
-    INTERNAL_SERVER_ERROR(HttpResponseStatus.INTERNAL_SERVER_ERROR.code(), "服务器内部异常，请联系管理员");
+    INTERNAL_SERVER_ERROR(HttpStatus.SC_INTERNAL_SERVER_ERROR, "服务器内部异常，请联系管理员");
 
     // 状态码
     private final int code;
