7918de7147367cf31277e758ac40ffb9612a0db9
Zeki <zeki.chen@tec-do.com>
2023-04-25 19:36:56 +0800

    opt

--- adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/BaseCampaignVO.java
+++ adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/BaseCampaignVO.java
@@ -4,6 +4,7 @@ import com.tecdo.starter.mp.vo.BaseVO;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.util.List;
 
@@ -11,11 +12,12 @@ import java.util.List;
  * Created by Zeki on 2023/3/15
  */
 @Data
+@EqualsAndHashCode(callSuper = true)
 @ApiModel(value = "CampaignGroupVO对象")
 public class BaseCampaignVO extends BaseVO {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty("AdGroup的id-name集合")
-	private List<BaseVO> baseAdGroupVOs;
+    @ApiModelProperty("AdGroup的id-name集合")
+    private List<BaseVO> baseAdGroupVOs;
 }
