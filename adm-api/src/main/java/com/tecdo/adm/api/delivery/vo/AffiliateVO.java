762c2775b7309d3afbc0f75801f71048bd363491
Zeki <zeki.chen@tec-do.com>
2023-05-14 14:47:52 +0800

    rollback

--- adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/AffiliateVO.java
+++ adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/AffiliateVO.java
@@ -1,13 +1,9 @@
 package com.tecdo.adm.api.delivery.vo;
 
-import com.tecdo.adm.api.delivery.entity.AffCountryBundleBList;
 import com.tecdo.adm.api.delivery.entity.Affiliate;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-
-import java.util.List;
 
 /**
  * Created by Zeki on 2023/3/15
@@ -18,8 +14,5 @@ import java.util.List;
 public class AffiliateVO extends Affiliate {
 
 	private static final long serialVersionUID = 1L;
-
-	@ApiModelProperty("渠道*国家*bundle黑名单集合")
-	private List<AffCountryBundleBList> affCountryBundleBLists;
 
 }
