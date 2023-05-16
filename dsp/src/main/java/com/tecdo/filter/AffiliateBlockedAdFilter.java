7fbde5ad97dc1baf5c4a768b5ae554151853f31a
Eric.Wang <eric.wang@tec-do.com>
2023-05-16 17:19:43 +0800

    [feat]添加渠道bdav和bapp过滤器

---
+++ dsp/src/main/java/com/tecdo/filter/AffiliateBlockedAdFilter.java
@@ -0,0 +1,40 @@
+package com.tecdo.filter;
+
+import com.tecdo.adm.api.delivery.entity.Affiliate;
+import com.tecdo.adm.api.delivery.entity.TargetCondition;
+import com.tecdo.adm.api.delivery.enums.ConditionEnum;
+import com.tecdo.domain.biz.dto.AdDTO;
+import com.tecdo.domain.openrtb.request.BidRequest;
+import com.tecdo.domain.openrtb.request.Imp;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class AffiliateBlockedAdFilter extends AbstractRecallFilter {
+
+    private static final String ATTR = ConditionEnum.AFFILIATE_BLOCKED_AD.getDesc();
+
+    @Override
+    public boolean doFilter(BidRequest bidRequest, Imp imp, AdDTO adDTO, Affiliate affiliate) {
+        TargetCondition condition = adDTO.getConditions().stream().filter(e -> ATTR.equals(e.getAttribute())).findFirst().orElse(null);
+        // 目前默认不开启这个过滤
+        if (condition == null) {
+            return true;
+        }
+        List<String> badv = bidRequest.getBadv();
+        if (CollectionUtils.isNotEmpty(badv) && badv.contains(adDTO.getCampaign().getDomain())) {
+            return false;
+        }
+        List<String> bapp = bidRequest.getBapp();
+        if (CollectionUtils.isNotEmpty(bapp) &&
+            bapp.contains(adDTO.getCampaign().getPackageName())) {
+            return false;
+        }
+
+        return true;
+
+    }
+}
