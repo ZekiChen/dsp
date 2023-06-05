1977bebbc7ef8aea012cf8953823cdb97fbcb6bd
Zeki <zeki.chen@tec-do.com>
2023-06-05 12:02:38 +0800

    dsp、job、script服务切读新doris集群

--- script/src/main/java/com/tecdo/mapper/doris/RequestMapper.java
+++ script/src/main/java/com/tecdo/mapper/doris/RequestMapper.java
@@ -7,10 +7,10 @@ import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
-@DS("doris-ods")
+@DS("doris-ads")
 public interface RequestMapper extends BaseMapper<String> {
 
-  @Select("select device_id from `pac_dsp_request_v2` WHERE report_hour >= #{start} and report_hour < #{end} and country = #{country}")
+  @Select("select device_id from `pac_dsp_request` WHERE report_hour >= #{start} and report_hour < #{end} and country = #{country}")
   List<String> listDeviceId(String country, String start, String end);
 
 }
