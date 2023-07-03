f4db26bc3c6be0e2320009fb0dbf38f19563b434
Zeki <zeki.chen@tec-do.com>
2023-07-03 14:53:22 +0800

    [fix] 修改任意AD状态报错bugfix

--- adm/src/main/java/com/tecdo/adm/delivery/wrapper/AdWrapper.java
+++ adm/src/main/java/com/tecdo/adm/delivery/wrapper/AdWrapper.java
@@ -28,32 +28,36 @@ public class AdWrapper extends EntityWrapper<Ad, AdVO> {
 	@Override
 	public AdVO entityVO(Ad ad) {
 		AdVO vo = Objects.requireNonNull(BeanUtil.copy(ad, AdVO.class));
-		AdGroup adGroup = AdGroupCache.getAdGroup(vo.getGroupId());
-		vo.setAdGroupName(adGroup.getName());
-		vo.setCampaignName(CampaignCache.getCampaign(adGroup.getCampaignId()).getName());
-		AdTypeEnum adType = AdTypeEnum.of(vo.getType());
-		Creative image;
-		switch (adType) {
-			case BANNER:
-				image = CreativeCache.getCreative(vo.getImage());
-				vo.setImageUrl(image.getUrl());
-				vo.setImageSize("w" + image.getWidth() + "h" + image.getHeight());
-				break;
-			case NATIVE:
-				image = CreativeCache.getCreative(vo.getImage());
-				Creative icon = CreativeCache.getCreative(vo.getIcon());
-				vo.setImageUrl(image.getUrl());
-				vo.setImageSize("w" + image.getWidth() + "h" + image.getHeight());
-				vo.setIconUrl(icon.getUrl());
-				vo.setIconSize("w" + icon.getWidth() + "h" + icon.getHeight());
-				break;
-			case VIDEO:
-				vo.setVideoUrl(CreativeCache.getCreative(vo.getVideo()).getUrl());
-				break;
-			case AUDIO:
-				throw new ServiceException("AD type not supported yet!");
-			default:
-				throw new ServiceException("Unknown ad type!");
+		if (vo.getGroupId() != null) {
+			AdGroup adGroup = AdGroupCache.getAdGroup(vo.getGroupId());
+			vo.setAdGroupName(adGroup.getName());
+			vo.setCampaignName(CampaignCache.getCampaign(adGroup.getCampaignId()).getName());
+		}
+		if (vo.getType() != null) {
+			AdTypeEnum adType = AdTypeEnum.of(vo.getType());
+			Creative image;
+			switch (adType) {
+				case BANNER:
+					image = CreativeCache.getCreative(vo.getImage());
+					vo.setImageUrl(image.getUrl());
+					vo.setImageSize("w" + image.getWidth() + "h" + image.getHeight());
+					break;
+				case NATIVE:
+					image = CreativeCache.getCreative(vo.getImage());
+					Creative icon = CreativeCache.getCreative(vo.getIcon());
+					vo.setImageUrl(image.getUrl());
+					vo.setImageSize("w" + image.getWidth() + "h" + image.getHeight());
+					vo.setIconUrl(icon.getUrl());
+					vo.setIconSize("w" + icon.getWidth() + "h" + icon.getHeight());
+					break;
+				case VIDEO:
+					vo.setVideoUrl(CreativeCache.getCreative(vo.getVideo()).getUrl());
+					break;
+				case AUDIO:
+					throw new ServiceException("AD type not supported yet!");
+				default:
+					throw new ServiceException("Unknown ad type!");
+			}
 		}
 		return vo;
 	}
