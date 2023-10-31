cc6f1d93892792462cfc65b7847eb15a5bee2b82
elwin <elwin.liu@tec-do.com>
2023-10-31 16:48:14 +0800

    [fix] 过滤bundle时改为用set比对

--- dsp/src/main/java/com/tecdo/filter/AutoBundleFilter.java
+++ dsp/src/main/java/com/tecdo/filter/AutoBundleFilter.java
@@ -1,5 +1,6 @@
 package com.tecdo.filter;
 
+import cn.hutool.core.util.StrUtil;
 import com.tecdo.adm.api.delivery.entity.Affiliate;
 import com.tecdo.adm.api.delivery.entity.TargetCondition;
 import com.tecdo.adm.api.delivery.enums.ConditionEnum;
@@ -8,6 +9,10 @@ import com.tecdo.domain.openrtb.request.BidRequest;
 import com.tecdo.domain.openrtb.request.Imp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Created by Zeki on 2023/10/23
@@ -25,8 +30,18 @@ public class AutoBundleFilter extends AbstractRecallFilter {
         TargetCondition autoBundleExceptCond = adDTO.getConditionMap().get(AUTO_BUNDLE_EXCEPT);
         String bundleId = bidRequest.getApp().getBundle();
 
-        // (白名单不为空 && 白名单包含bundle) || (黑名单为空 || 黑名单不包含bundle)
-        return (autoBundleExceptCond != null && autoBundleExceptCond.getValue().contains(bundleId)) ||
-                (autoBundleCond == null || !autoBundleCond.getValue().contains(bundleId));
+        // 黑白名单用集合表示
+        Set<String> blackList = new HashSet<>();
+        Set<String> whiteList = new HashSet<>();
+
+        if (autoBundleCond != null && StrUtil.isNotBlank(autoBundleCond.getValue())) {
+            Collections.addAll(blackList, autoBundleCond.getValue().split(","));
+        }
+        if (autoBundleExceptCond != null && StrUtil.isNotBlank(autoBundleExceptCond.getValue())) {
+            Collections.addAll(whiteList, autoBundleExceptCond.getValue().split(","));
+        }
+
+        // 白名单包含bundle || 黑名单不包含bundle
+        return blackList.contains(bundleId) || !whiteList.contains(bundleId);
     }
 }
