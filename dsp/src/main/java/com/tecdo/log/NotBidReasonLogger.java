e9f2b4c042a35d00089170cf08d80e281972ddb5
eric.wang <eric.wang@tec-do.com>
2023-12-02 17:22:40 +0800

    [bugfix]添加了一个定时清理过期not bid数据的定时任务，修复非正常情况下，not bid logger记录的数据没有被消费或清除

--- dsp/src/main/java/com/tecdo/log/NotBidReasonLogger.java
+++ dsp/src/main/java/com/tecdo/log/NotBidReasonLogger.java
@@ -2,20 +2,29 @@ package com.tecdo.log;
 
 import com.tecdo.domain.biz.log.NotBidReasonLog;
 import com.tecdo.util.JsonHelper;
-
+import java.util.Calendar;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentHashMap.KeySetView;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class NotBidReasonLogger {
   private final static Logger logger = LoggerFactory.getLogger(NotBidReasonLogger.class);
 
   private final static Logger notBidReasonLog = LoggerFactory.getLogger("not_bid_reason_log");
 
+  private final static ScheduledExecutorService service =
+      Executors.newSingleThreadScheduledExecutor();
+
   private final static ConcurrentHashMap<String, LinkedBlockingQueue<NotBidReasonLog>> map =
     new ConcurrentHashMap<>();
+
+  private static final int CLEAR_INTERVAL = 600;
+  private static final int NEED_CLEAR_DATA_TIME = 600;
 
   public static void log(String bidId, Integer adId, String reason) {
     NotBidReasonLog log = new NotBidReasonLog(bidId, adId, reason);
@@ -34,6 +43,28 @@ public class NotBidReasonLogger {
     LinkedBlockingQueue<NotBidReasonLog> logQueue = map.remove(bidId);
     if (logQueue != null && !logQueue.isEmpty()) {
       logQueue.clear();
+    }
+  }
+
+  public static void init() {
+    service.scheduleAtFixedRate(new ClearTask(), 60, CLEAR_INTERVAL, TimeUnit.SECONDS);
+  }
+
+
+  static class ClearTask implements Runnable {
+
+    @Override
+    public void run() {
+      Calendar calendar = Calendar.getInstance();
+      calendar.add(Calendar.SECOND, -NEED_CLEAR_DATA_TIME);
+      long needClearTime = calendar.getTimeInMillis();
+      KeySetView<String, LinkedBlockingQueue<NotBidReasonLog>> keys = map.keySet();
+      for (String key : keys) {
+        long createStamp = Long.parseLong(key.substring(32, 45));
+        if (createStamp < needClearTime) {
+          clear(key);
+        }
+      }
     }
   }
 
