ddbf559a865f65af2e99b6d6ce9edfb408517080
elwin <elwin.liu@tec-do.com>
2024-01-16 12:05:28 +0800

    [feat] vlion报表自动写入，引入vlion提供的api

--- job/src/main/java/com/tecdo/job/handler/affiliate/VlionJob.java
+++ job/src/main/java/com/tecdo/job/handler/affiliate/VlionJob.java
@@ -1,5 +1,8 @@
 package com.tecdo.job.handler.affiliate;
 
+import cn.hutool.core.net.url.UrlBuilder;
+import com.ejlchina.data.Mapper;
+import com.ejlchina.okhttps.OkHttps;
 import com.tecdo.adm.api.delivery.dto.SpentDTO;
 import com.tecdo.job.foreign.feishu.AffReport;
 import com.tecdo.job.mapper.DspReportMapper;
@@ -32,19 +35,41 @@ public class VlionJob {
     private final String sheetId = "4abee8";
     private final String sheetToken = "E0gvsATOGhwcHptB9w5cOHfSnsf";
     private final String unitRange = "?!A3:A3";
-    private final String range = "?!A3:C3";
+    private final String range = "?!A3:G3";
     private final Integer affId = 134;
+    private final String vlionApiUrl = "http://report.vlion.cn/adx_dsp/Tecdo_DSP/8b5f6e54c5c3647f2deed77ee10f867c";
 
     @XxlJob("FeishuAff134Job")
     public void dspReport() {
         XxlJobHelper.log("获取Vlion doris库dsp_report表前一天数据，写入飞书文档");
-        LocalDate today = TimeZoneUtils.dateInChina();
+        LocalDate today = LocalDate.now();
 
         String targetDate = affReport.dateFormat(today);
-        SpentDTO spent = reportMapper.getImpCostForAffUTC8(targetDate, affId);
+        SpentDTO dspSpent = reportMapper.getImpCostForAffUTC0(targetDate, affId);
+        SpentDTO affSpent = getVlionSpent(targetDate);
 
-        affReport.postData(today, sheetId, sheetToken, spent, null, costRatio, impRatio, range);
+        affReport.postData(today, sheetId, sheetToken, dspSpent, affSpent, costRatio, impRatio, range);
         affReport.unitFormatter(sheetId, sheetToken, unitRange);
+
+        affReport.insertGapReport(affId, targetDate, dspSpent, affSpent);
+    }
+
+    private SpentDTO getVlionSpent(String date) {
+        String url =  UrlBuilder.of(vlionApiUrl)
+                .addQuery("start_date", date)
+                .addQuery("end_date", date)
+                .toString();
+        Mapper result = OkHttps.sync(url)
+                .bodyType(OkHttps.JSON)
+                .get()
+                .getBody()
+                .toMapper();
+        Mapper data = result.getArray("data").getMapper(0);
+        SpentDTO affSpent = new SpentDTO();
+        affSpent.setImp(data.getLong("imp"));
+        affSpent.setCost(data.getDouble("revenue"));
+
+        return affSpent;
     }
 
 }
