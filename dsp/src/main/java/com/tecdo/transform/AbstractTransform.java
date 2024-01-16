662251ca22283c43a3bd32ecb60575d4d3e64550
Zeki <zeki.chen@tec-do.com>
2024-01-16 10:13:21 +0800

    [fix] video强跳选择逻辑调整

--- dsp/src/main/java/com/tecdo/transform/AbstractTransform.java
+++ dsp/src/main/java/com/tecdo/transform/AbstractTransform.java
@@ -149,7 +149,7 @@ public abstract class AbstractTransform implements IProtoTransform {
     private boolean isForceJump(String forceLink, AdDTOWrapper wrapper) {
         AdDTO adDTO = wrapper.getAdDTO();
         return (Objects.equals(adDTO.getAd().getType(), AdTypeEnum.BANNER.getType())
-                || (Objects.equals(adDTO.getAd().getType(), AdTypeEnum.VIDEO.getType())) && videoForceEnabled)
+                || (wrapper.getImage() != null && videoForceEnabled))
                 && forceBannerEnable()
                 && adDTO.getAdGroup().getForceJumpEnable()
                 && StringUtils.isNotBlank(forceLink)
@@ -367,7 +367,7 @@ public abstract class AbstractTransform implements IProtoTransform {
                 }
                 break;
             case VIDEO:
-                if (isForce && adDTO.getAd().getImage() != null) {
+                if (isForce) {
                     Object forceBannerAdm = buildForceBannerAdm(wrapper, bidRequest, affiliate, adDTO, sign, impTrackList, clickTrackList, deepLink, forceLink, clickUrl, forceJudgeUrl, collectFeatureUrl, collectCodeUrl, collectErrorUrl);
                     adm = AdmGenerator.forceVideoAdm(adDTO.getAd().getId(),
                             adDTO.getCreativeMap().get(adDTO.getAd().getVideo()),
