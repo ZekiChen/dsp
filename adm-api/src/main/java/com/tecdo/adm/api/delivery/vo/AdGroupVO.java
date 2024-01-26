f2148b57b964b72cf1ea5ac3e40aad3e78213f7b
Zeki <zeki.chen@tec-do.com>
2024-01-26 18:18:33 +0800

    [fix] 新增时，condition过滤非空条件

--- adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/AdGroupVO.java
+++ adm-api/src/main/java/com/tecdo/adm/api/delivery/vo/AdGroupVO.java
@@ -8,6 +8,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.BeanUtils;
 
 import java.util.List;
@@ -21,43 +22,45 @@ import java.util.stream.Collectors;
 @ApiModel("AdGroupVO对象")
 public class AdGroupVO extends AdGroup {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	@ApiModelProperty("广告活动名称")
-	private String campaignName;
-	@ApiModelProperty("渠道名称集")
-	private String affiliateNames;
-	@ApiModelProperty("国家集")
-	private String countries;
-	@ApiModelProperty("定向条件集")
-	List<TargetConditionVO> conditionVOs;
-	@ApiModelProperty("花费是否到达预算上限标识")
-	private boolean isCostFull;
-	@ApiModelProperty("双阶段出价模式")
-	private List<MultiBidStrategyVO> strategyVOs;
+    @ApiModelProperty("广告活动名称")
+    private String campaignName;
+    @ApiModelProperty("渠道名称集")
+    private String affiliateNames;
+    @ApiModelProperty("国家集")
+    private String countries;
+    @ApiModelProperty("定向条件集")
+    List<TargetConditionVO> conditionVOs;
+    @ApiModelProperty("花费是否到达预算上限标识")
+    private boolean isCostFull;
+    @ApiModelProperty("双阶段出价模式")
+    private List<MultiBidStrategyVO> strategyVOs;
 
-	@ApiModelProperty(value = "竞价策略", notes = "BidStrategyEnum")
-	private String multiBidStrategy;
-	@ApiModelProperty("操作价格")
-	private String multiOptPrice;
+    @ApiModelProperty(value = "竞价策略", notes = "BidStrategyEnum")
+    private String multiBidStrategy;
+    @ApiModelProperty("操作价格")
+    private String multiOptPrice;
 
-	public List<MultiBidStrategy> listStrategies() {
-		List<MultiBidStrategy> strategies = strategyVOs.stream().map(e -> {
-			MultiBidStrategy strategy = new MultiBidStrategyVO();
-			BeanUtils.copyProperties(e, strategy);
-			return strategy;
-		}).collect(Collectors.toList());
-		strategies.forEach(e -> e.setAdGroupId(getId()));
-		return strategies;
-	}
+    public List<MultiBidStrategy> listStrategies() {
+        List<MultiBidStrategy> strategies = strategyVOs.stream().map(e -> {
+            MultiBidStrategy strategy = new MultiBidStrategyVO();
+            BeanUtils.copyProperties(e, strategy);
+            return strategy;
+        }).collect(Collectors.toList());
+        strategies.forEach(e -> e.setAdGroupId(getId()));
+        return strategies;
+    }
 
-	public List<TargetCondition> listCondition() {
-		List<TargetCondition> conditions = conditionVOs.stream().map(e -> {
-			TargetCondition condition = new TargetCondition();
-			BeanUtils.copyProperties(e, condition);
-			return condition;
-		}).collect(Collectors.toList());
-		conditions.forEach(e -> e.setAdGroupId(getId()));
-		return conditions;
-	}
+    public List<TargetCondition> listCondition() {
+        List<TargetCondition> conditions = conditionVOs.stream()
+                .filter(e -> Strings.isNotBlank(e.getValue()))
+                .map(e -> {
+                    TargetCondition condition = new TargetCondition();
+                    BeanUtils.copyProperties(e, condition);
+                    return condition;
+                }).collect(Collectors.toList());
+        conditions.forEach(e -> e.setAdGroupId(getId()));
+        return conditions;
+    }
 }
