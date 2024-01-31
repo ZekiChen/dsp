febb64cfe1cddf029286c7a40fa6c0a6056e598a
Zeki <zeki.chen@tec-do.com>
2024-01-31 19:14:04 +0800

    [fix] 适配为解析url文件中多个csv文件

--- job/src/main/java/com/tecdo/job/handler/fraud/pixalate/PostBidSinkToObsJob.java
+++ job/src/main/java/com/tecdo/job/handler/fraud/pixalate/PostBidSinkToObsJob.java
@@ -2,6 +2,7 @@ package com.tecdo.job.handler.fraud.pixalate;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.net.url.UrlBuilder;
+import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.StrUtil;
 import com.ejlchina.okhttps.HTTP;
 import com.ejlchina.okhttps.HttpResult;
@@ -14,16 +15,25 @@ import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.OkHttpClient;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 /**
  * Created by Elwin on 2024/1/25
@@ -42,29 +52,54 @@ public class PostBidSinkToObsJob {
     public void postBidSinkToObs() {
         XxlJobHelper.log("Job start: pixalate postBid data is being pulled from api and sink to OBS");
         String date = DateUtil.yesterday().toDateStr();
-        int maxLoopTime = 20; // 避免无限循环的情况出现
         int order = 0;
+        long start = System.currentTimeMillis();
 
-        while (order < maxLoopTime) {
-            String urlStr = buildUrl(date, date, order); // 构建请求
-            String csvUrl = reqCsvFromPixalate(urlStr); // csv文件url
-            if (isCsvEmpty(csvUrl)) break; // 若文件为空，说明当天的数据已经遍历完毕，跳出
+        List<CompletableFuture<Void>> futures = new ArrayList<>();
+        String requestUrl = buildUrl(date, date, order); // 构建请求
+        String batchCsvUrl = reqCsvFromPixalate(requestUrl); // 发送请求，返回一个csv文件的url
+        ThreadUtil.sleep(10, TimeUnit.SECONDS);  // 太快打开会返回404
+        List<String> urls = readEveryCsvUrlFromBatchCsvUrl(start, batchCsvUrl);
 
-            /**
-             * test
-             */
-/*            String csvUrl = "https://dashboardcdn.pixalate.com/www/exported/tdo/2024-01-29/372rmf6vivsbhla16u1m5oucmc/Report_for_2024-01-28.csv";
-            order = 100;*/
-
-
+        for (String url : urls) {
+            if (isCsvEmpty(url)) break; // 若文件为空，说明当天的数据已经遍历完毕，跳出
             // 在线程池中读区csv写入obs
             final int curOrder = order;
-            final String curCsvUrl = csvUrl;
-            threadPool.execute(() -> {
-                uploadToHuaWeiOBS(curCsvUrl, date, curOrder);
-            });
+            final String curCsvUrl = url;
+            futures.add(CompletableFuture.runAsync(() ->
+                    uploadToHuaWeiOBS(curCsvUrl, date, curOrder), threadPool.getExecutor()));
             order++;
         }
+
+        try {
+            CompletableFuture.allOf(futures.toArray(new CompletableFuture<?>[0])).get(3600, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            XxlJobHelper.log("Job execute error");
+            throw new RuntimeException(e);
+        }
+
+        XxlJobHelper.log("Job end: pixalate postBid data sink to OBS successfully: {}s",
+                (System.currentTimeMillis() - start) / 1000);
+    }
+
+    @NotNull
+    private static List<String> readEveryCsvUrlFromBatchCsvUrl(long start, String batchCsvUrl) {
+        List<String> urls = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(batchCsvUrl).openStream()))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.endsWith(".csv")) {
+                    urls.add(line);
+                }
+            }
+        } catch (MalformedURLException e) {
+            XxlJobHelper.log("url open stream error, spent: {}s, batch csv url: {}", (System.currentTimeMillis() - start) / 1000, batchCsvUrl);
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            XxlJobHelper.log("read batch csv file error, spent: {}s, batch csv url: {}", (System.currentTimeMillis() - start) / 1000, batchCsvUrl);
+            throw new RuntimeException(e);
+        }
+        return urls;
     }
 
     /**
@@ -75,16 +110,16 @@ public class PostBidSinkToObsJob {
      * @return 请求url
      */
     public String buildUrl(String startDate, String endDate, Integer start) {
-        int maxItemSize = 1000000; // 单个文件最大item数量
         String baseUrl = "https://api.pixalate.com/api/v2/analytics/reports/default?";
         String simpleSql = "advertiserId,campaignId,city,countryCode,day,deviceBrandName," +
                 " deviceModelId,deviceMarketingName, fraudType, kv4,kv7,kv18,kv19,kv26, publisherId" +
                 " WHERE day>='" + startDate + "' AND day<='" + endDate + "'";
-        String url = UrlBuilder.of(baseUrl).addQuery("q", simpleSql)
+//                " WHERE fraudType != '' AND day>='" + startDate + "' AND day<='" + endDate + "'";
+        return UrlBuilder.of(baseUrl).addQuery("q", simpleSql)
                 .addQuery("exportUri", true)
-                .addQuery("start", start * maxItemSize)
+//                .addQuery("start", start * maxItemSize)
+                .addQuery("isLargeResultSet", true)
                 .toString();
-        return url;
     }
 
     private void uploadToHuaWeiOBS(String url, String date, Integer order) {
